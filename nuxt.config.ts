2c9a37076379027cc30dee8030c81ea804cb38f6
lcAlberto <lcalbertomatoso2012@gmail.com>
2024-05-12 16:07:33 -0300

    Criação do darkmode e ajustes na sidebar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  app: {
-      head: {
-          link: [
-              { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css' }
-          ],
-          meta: [
-              { charset: 'utf-8' },
-              { 'data-theme': 'light' },
-          ]
-      }
-  },
-  runtimeConfig: {
-      // apiSecret: '',
-      public: {
-          apiSecret: '',
-          apiBase: ''
-      }
+    devtools: {enabled: true},
+    app: {
+        head: {
+            link: [
+                {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'}
+            ],
+            meta: [
+                {charset: 'utf-8'},
+                {'data-theme': 'my-theme'},
+            ]
+        }
     },
-  modules: [
-      '@nuxtjs/eslint-module',
-      '@nuxtjs/tailwindcss',
-      '@pinia/nuxt'
-  ],
+    runtimeConfig: {
+        // apiSecret: '',
+        public: {
+            apiSecret: '',
+            apiBase: ''
+        }
+    },
+    modules: [
+        '@nuxtjs/eslint-module',
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt'
+    ],
     plugins: [
         // '~/plugins/apiClient.ts',
     ],
